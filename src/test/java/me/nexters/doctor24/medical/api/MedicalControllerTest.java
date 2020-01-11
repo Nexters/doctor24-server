9676a09076a0abeaaf599a0637d3353e5d670417
manki.kim <manki.kim@navercorp.com>
2020-01-11 15:21:25 +0900

    #27 swagger 정보 추가 및 프로퍼티 분리

--- src/test/java/me/nexters/doctor24/medical/api/MedicalControllerTest.java
+++ src/test/java/me/nexters/doctor24/medical/api/MedicalControllerTest.java
@@ -1,5 +1,8 @@
 package me.nexters.doctor24.medical.api;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,22 +10,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 @AutoConfigureMockMvc
 @SpringBootTest
 class MedicalControllerTest {
-	private static final String PRODUCT_API = linkTo(MedicalController.class).toString();
 
 	@Autowired
 	private MockMvc mockMvc;
-
-	@Test
-	void name() {
-		System.out.println(PRODUCT_API);
-	}
 
 	// TODO 스펙 나오면 정의할 것
 	@Test
