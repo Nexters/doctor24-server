7a1787b806fbb2fe0791b6e2c28e8c77cd6589d3
manki.kim <manki.kim@navercorp.com>
2020-02-28 16:20:08 +0900

    비교 성능 개선 처리

--- src/main/java/me/nexters/doctor24/medical/corona/analyzer/CoronaClinicDiffAnalyzer.java
+++ src/main/java/me/nexters/doctor24/medical/corona/analyzer/CoronaClinicDiffAnalyzer.java
@@ -1,8 +1,9 @@
 package me.nexters.doctor24.medical.corona.analyzer;
 
-import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.stream.Collectors;
 
 import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,23 +29,21 @@ public class CoronaClinicDiffAnalyzer {
 
 	public Flux<NewCoronaClinic> getNewClinics() {
 		Mono<String> html = coronaClinicsInvoker.getClinicPageHtml();
+		CompletableFuture<Set<String>> existsClinics = CompletableFuture.supplyAsync(
+			() -> coronaRepository.findAll().collectList().block().stream()
+				.map(CoronaHospital::getName)
+				.collect(Collectors.toSet()), inquiryPool);
 
+		Set<String> resultSet = existsClinics.join();
 		return html.map(Jsoup::parse)
 			.flatMapMany(doc -> Flux.fromIterable(doc.getElementsByClass("tb_center").select("tr"))
 				.map(element -> element.select("td").get(2).text().replace("*(검체채취 가능)", ""))
 				.map(String::trim))
 			.flatMap(name -> {
-				if (notExist(name)) {
+				if (!resultSet.contains(name)) {
 					return Flux.just(NewCoronaClinic.of(name));
 				}
 				return Flux.empty();
 			});
-	}
-
-	private boolean notExist(String clinic) {
-		CompletableFuture<CoronaHospital> future = CompletableFuture.supplyAsync(
-			() -> coronaRepository.findByName(clinic).blockFirst(), inquiryPool);
-
-		return Objects.isNull(future.join());
 	}
 }
