b75307bbbbaa1a74845ad1860d52ffd0f7477961
Junho Park <pjh374968@gmail.com>
2020-03-08 01:24:29 +0900

    #114 root gradle 수정 및 정리
    각 모듈 별 빌드 가능

--- todoc-api/src/test/java/me/nexters/doctor24/medical/external/holiday/HolidayInquiresTest.java
+++ todoc-api/src/test/java/me/nexters/doctor24/medical/external/holiday/HolidayInquiresTest.java
@@ -1,10 +1,10 @@
 package me.nexters.doctor24.medical.external.holiday;
 
 import me.nexters.doctor24.medical.external.holiday.dto.HolidayRaw;
-import org.apache.commons.collections.CollectionUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
