63b784cb4987409e29efa91d31d91119087f3c47
JunHoPark93 <pjh374968@gmail.com>
2020-03-09 11:17:36 +0900

    #116 Pharmacy builder 빼먹은것 추가

--- todoc-batch/src/main/java/me/nexters/doctor24/batch/processor/util/PharmacyParser.java
+++ todoc-batch/src/main/java/me/nexters/doctor24/batch/processor/util/PharmacyParser.java
@@ -25,6 +25,7 @@ public class PharmacyParser {
 			.address(pharmacyRaw.getAddress())
 			.phone(pharmacyRaw.getPhone())
 			.rowWriteDate(LocalDateTime.now())
+				.isManaged(false)
 			.build();
 	}
 
