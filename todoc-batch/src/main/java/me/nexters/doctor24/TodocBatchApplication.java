bfca5e3df20db41b53b8446b905f2206efec23dc
JunHoPark93 <pjh374968@gmail.com>
2020-03-10 07:39:43 +0900

    @EnableScheduling 빠진것 추가

--- todoc-batch/src/main/java/me/nexters/doctor24/TodocBatchApplication.java
+++ todoc-batch/src/main/java/me/nexters/doctor24/TodocBatchApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import reactivefeign.spring.config.EnableReactiveFeignClients;
 
+@EnableScheduling
 @EnableReactiveFeignClients
 @EnableBatchProcessing
 @EnableReactiveMongoRepositories(basePackages = "me.nexters.domain")
