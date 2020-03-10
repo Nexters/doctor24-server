06e77e98fae86f761b0e88b2f18174a5c2e85cd5
JunHoPark93 <pjh374968@gmail.com>
2020-03-10 11:25:04 +0900

    스키마 변경

--- todoc-batch/src/main/java/me/nexters/doctor24/batch/scheduler/JobScheduler.java
+++ todoc-batch/src/main/java/me/nexters/doctor24/batch/scheduler/JobScheduler.java
@@ -15,7 +15,7 @@ public class JobScheduler {
 	private final Job medicalJob;
 	private final JobLauncher jobLauncher;
 
-	@Scheduled(cron = "0 0 08 * * ?")
+	@Scheduled(cron = "0 0 12 * * ?")
 	public void getMedicalJob() {
 		try {
 			JobParameters jobParameters = new JobParametersBuilder()
